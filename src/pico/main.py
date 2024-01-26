d9889ddb05b614f696af11fe56a891c971593eea
ghostiek <patricktawil1@hotmail.com>
2024-01-25 19:17:40 -0500

    Sending Sensor Data over MQTT to Pi4

--- src/pico/main.py
+++ src/pico/main.py
@@ -1,15 +1,15 @@
 import json
 
 from sensor import Ultrasonic
-from time import sleep
+import utime
 import network
 from umqtt.simple import MQTTClient
 
-with open("mqttconfig.json", "r") as mqtt_config:
-    mqtt_creds = json.load(mqtt_config)
-
 with open("wifi_config.json", "r") as wifi_config:
     wifi_creds = json.load(wifi_config)
+
+with open("mqtt_config.json", "r") as mqtt_config:
+    mqtt_creds = json.load(mqtt_config)
 
 
 def do_connect():
@@ -25,43 +25,25 @@ def do_connect():
 
 do_connect()
 
-mqtt_server = str.encode(mqtt_creds["server"])
+mqtt_ip = str.encode(mqtt_creds["ip"])
+mqtt_port = mqtt_creds["port"]
 client_id = str.encode(mqtt_creds["client_id"])
 topic_pub = str.encode(mqtt_creds['topic_pub'])
 username = str.encode(mqtt_creds["username"])
 password = str.encode(mqtt_creds["password"])
 
 
-def mqtt_connect():
-    client = MQTTClient(client_id,
-                        mqtt_server,
-                        user=username,
-                        password=password,
-                        keepalive=3600,
-                        ssl=True,
-                        ssl_params={'server_hostname': mqtt_creds["server"]})
-    client.connect()
-    print('Connected to %s MQTT Broker' % (mqtt_server))
-    return client
-
-
-def reconnect():
-    print('Failed to connect to the MQTT Broker. Reconnecting...')
-    sleep(5)
-    machine.reset()
-
-
-try:
-    client = mqtt_connect()
-except OSError as e:
-    reconnect()
-
+client = MQTTClient(client_id, mqtt_ip, port=mqtt_port, user=username, password=password)
 sns = Ultrasonic(17, 16)
 
+
 while True:
+    client.connect()
     distance = sns.distance_in_cm()
     if distance:
         print('Distance:', distance, 'cm')
         client.publish(topic_pub, str.encode(str(distance)))
-    sleep(5)
+    client.disconnect()
+    utime.sleep(5)
+    
 

61495e177852345dfdb4efe2ec99e8819a52d85e
ghostiek <patricktawil1@hotmail.com>
2024-01-31 17:02:51 -0500

    added viz

--- src/Pi/readme/visualisation.py
+++ src/Pi/readme/visualisation.py
@@ -1,11 +1,17 @@
 import matplotlib.pyplot as plt
+import matplotlib.dates as dts
 import src.Pi.db.db_utils as db
 import json
 import os
 from pathlib import Path
 
 
-def run():
+def get_data(cached=False):
+    path = "src/Pi/readme/data.json"
+    if cached:
+        with open(path, "r") as data_file:
+            data = json.load(data_file)
+        return data
     dir_path = os.path.dirname(os.path.realpath(__file__))
     path = Path(dir_path)
     parent_path = path.parent.absolute()
@@ -14,14 +20,19 @@ def run():
     data = db.read_data(conn, cur)
     conn.close()
 
-    with open("data.json", "w") as data_file:
+    with open(path, "w") as data_file:
         json.dump(data, data_file, indent=4, sort_keys=True, default=str)
+    return data
 
-    x = [0, 1, 2, 3]
-    y = [0, 2, 4, 6]
-    plt.plot(x, y)
+
+def plot(data):
+    dates0 = [x[1] for x in data]
+    dates = dts.date2num(dates0)
+    distance = [x[2] for x in data]
+    ax = plt.plot_date(dates, distance)
+    plt.xlim(dates[-30], dates[-5])
     plt.show()
 
 
-if __name__ == "__main__":
-    run()
+data = get_data(True)
+plot(data)

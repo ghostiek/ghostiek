27a1954bc8f08b774b88de556beea032a2d3ccb0
ghostiek <patricktawil1@hotmail.com>
2024-01-31 16:20:14 -0500

    fix code

--- src/Pi/readme/visualisation.py
+++ src/Pi/readme/visualisation.py
@@ -1,17 +1,24 @@
 import matplotlib.pyplot as plt
 import src.Pi.db.db_utils as db
 import json
+import os
 
 
-conn = db.connect_db()
-cur = conn.cursor()
-data = db.read_data(conn, cur)
-conn.close()
+def run():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    conn = db.connect_db()
+    cur = conn.cursor()
+    data = db.read_data(conn, cur)
+    conn.close()
 
-with open("data.json", "w") as data_file:
-    json.dump(data, data_file)
+    with open("data.json", "w") as data_file:
+        json.dump(data, data_file)
 
-x = [0,1,2,3]
-y=[0,2,4,6]
-plt.plot(x,y)
-plt.show()
+    x = [0, 1, 2, 3]
+    y = [0, 2, 4, 6]
+    plt.plot(x, y)
+    plt.show()
+
+
+if __name__ == "__main__":
+    run()

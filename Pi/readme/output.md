89713f116c7b910a494e127161bedea1bad5e4a3
ghostiek <patricktawil1@hotmail.com>
2024-02-03 17:22:46 -0500

    Added plot + readme generation

--- Pi/readme/output.md
+++ Pi/readme/output.md
@@ -1,14 +1,4 @@
-import os
-from pathlib import Path
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-path = Path(dir_path)
-parent_path = path.parent.parent.parent.absolute()
-readme_path = f"{parent_path}/README.md"
-output_path = f"{path}/output.md"
-
-
-readme_text = """
 # Hello there ( ´◔ ω◔`) ノシ
 
 I'm a Data Scientist and I love learning about Statistics.
@@ -21,9 +11,3 @@ I'm a Data Scientist and I love learning about Statistics.
   <img alt="Shows a black logo in light color mode and a white one in dark color mode." src="">
 </picture>
 
-"""
-
-with open(output_path, "w", encoding="utf-8") as output_file:
-    output_file.write(readme_text)
-
-print(readme_text)
--- Pi/readme/visualisation.py
+++ Pi/readme/visualisation.py
@@ -4,6 +4,8 @@ import Pi.db.db_utils as db
 import json
 import os
 from pathlib import Path
+import seaborn as sns
+import pandas as pd
 
 
 def get_data(cached=False):
@@ -35,5 +37,58 @@ def plot(data):
     plt.show()
 
 
+def pretty_plot(data):
+    dates = [x[1] for x in data]
+    distance = [x[2] for x in data]
+    df = pd.DataFrame({"TimestampString":dates, "Distance": distance})
+    df["Timestamp"] = pd.to_datetime(df["TimestampString"], format="%Y-%m-%d %H:%M:%S")
+    # Add the line over the area with the plot function
+    fig = plt.figure(figsize=[7, 5])
+    ax = plt.subplot(111)
+
+    # Fill the area with fill_between
+    l = ax.fill_between(df['Timestamp'], df['Distance'], alpha=0.2)
+
+    # Control the title of each facet
+    # set the basic properties
+    ax.set_xlabel('Timestamp')
+    ax.set_ylabel('Distance from Computer')
+    ax.set_title("Time Spent on Computer")
+    ax.set_xlim(df["Timestamp"].iloc[-50], df["Timestamp"].iloc[-10])
+    ax.set_ylim(0, max(distance[-50:-10])+30)
+    # set the grid on
+    ax.grid('on')
+
+    l.set_facecolors([[.5, .5, .8, .3]])
+    #
+    # # change the edge color (bluish and transparentish) and thickness
+    l.set_edgecolors([[0, 0, .5, .3]])
+    l.set_linewidths([3])
+
+    # remove tick marks
+    ax.xaxis.set_tick_params(size=0)
+    ax.yaxis.set_tick_params(size=0)
+
+    # change the color of the top and right spines to opaque gray
+
+    ax.spines['right'].set_color((.8, .8, .8))
+    ax.spines['top'].set_color((.8, .8, .8))
+
+    # tweak the axis labels
+    xlab = ax.xaxis.get_label()
+    ylab = ax.yaxis.get_label()
+
+    xlab.set_style('italic')
+    xlab.set_size(10)
+    ylab.set_style('italic')
+    ylab.set_size(10)
+
+    # tweak the title
+    ttl = ax.title
+    ttl.set_weight('bold')
+    # Show the graph
+    plt.show()
+
+
 data = get_data(True)
-plot(data)
+pretty_plot(data)

4fba558ae88facc54d1472ab0eb5331248424f3b
ghostiek <patricktawil1@hotmail.com>
2024-02-21 00:44:46 -0500

    making a prettier plot

--- Pi/readme/time_on_pc.py
+++ Pi/readme/time_on_pc.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 def get_cumulative_times(data: pd.DataFrame, delay: int = 0):
     max_time = datetime.today() - timedelta(days=delay)
     max_time = max_time.replace(hour=0, minute=0, second=0, microsecond=0)
-    current_time = max_time - timedelta(days=1) - timedelta(days=delay)
+    current_time = max_time - timedelta(days=1+delay)
 
     # Turn dataframe to list of list for optimized looping, 10x speedup over iloc loop
     elements = list(map(list, data.itertuples(index=False)))
@@ -34,3 +34,26 @@ def get_cumulative_times(data: pd.DataFrame, delay: int = 0):
     else:
         time_off_pc += max_time - current_time
     return time_on_pc, time_off_pc
+
+
+if __name__ == "__main__":
+    from Pi.readme.visualisation import get_data
+    import matplotlib.pyplot as plt
+    data = get_data(False, 2)
+    result = -data['Timestamp'].where(data['on_pc']).diff(periods=-1).sum()
+    print(result)
+
+    x = get_cumulative_times(data, 1)
+    # Not on PC
+    time1 = x[0].total_seconds()
+    # On PC
+    time2 = x[1].total_seconds()
+    total = time1+time2
+    #plt.bar(["On PC", "Not On PC"], [x1.total_seconds() for x1 in x])
+    # Make thinner, remove grid, add title maybe? fix colors
+    plt.barh([0], (time1 + time2) / total, color="black")
+    plt.barh([0], time2/total, color="red")
+
+
+    #my_pie, _, _ = plt.pie([x1.total_seconds() for x1 in x], radius=1.2, colors=["red", "black"], autopct="%.1f%%")
+    plt.show()

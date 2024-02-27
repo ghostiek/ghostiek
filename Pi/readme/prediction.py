43c9135a554c31723507491a1adf95b691a00ae6
ghostiek <patricktawil1@hotmail.com>
2024-02-27 00:47:28 -0500

    fixing impl

--- Pi/readme/prediction.py
+++ Pi/readme/prediction.py
@@ -1,19 +1,19 @@
 # Currently we don't have much data, what would make the most sense would be to simply use a 4 day moving average will
 # iterate more on it in the future
 import pandas as pd
-from datetime import date
+from datetime import date, timedelta
 from Pi.db.db_utils import connect_db, read_aggregate_data
 
-def moving_average_prediction(df: pd.DataFrame, days: int):
-    if days < 1:
-        raise ValueError("Days parameter needs to be greater than 0")
+
+def moving_average_prediction(df: pd.DataFrame):
+    return df["percent_time_on_pc"].sum()/df.shape[0]
 
 
 if __name__ == "__main__":
     conn = connect_db()
     dt_now = date.today()
     data = read_aggregate_data(conn, dt_now)
-    result = moving_average_prediction(data, 1)
+    result = moving_average_prediction(data)
     print(result)
 
 
